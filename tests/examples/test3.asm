f8aad003d392584d6937f2b8bb96a7bf7b7f1b36
Dinesh Chukkala <dineshchukkala169@gmail.com>
2022-09-09 06:14:07 +0530

    added a example file to compute sum of first n natural numbers

--- tests/examples/test3.asm
+++ tests/examples/test3.asm
@@ -1,24 +1,28 @@
-ldc 0x100 ; initial value
-a2sp ;stack move
-ldc times ; count
+; Program to Calculate `1 + 2 + ... + times`
+
+ldc 0x100           ; initial value
+a2sp                ;stack move
+ldc times           ; count
 adj -1
 stl 0
-LOOP: ; loop start
-    ldl 0 ; load left count
-    brz LOOP_OUT ; check
-    ldc 1 ; decrement
-    sub ; left count - 1
-    stl 0 ; store count left
-    ldc result ; load memory addr of result
-    ldnl 0 ; load value of data
-    adc increment ; add increment
-    ldc result ; load addr, and B = A
-    stnl 0 ; store again
+
+LOOP:               ; loop start
+    ldl 0           ; load left count
+    brz LOOP_OUT    ; check
+    ldc result      ; load memory addr of result
+    ldnl 0          ; load value of data
+    ldl 0
+    add
+    ldc result      ; load addr, and B = A
+    stnl 0          ; store again
+    ldl 0
+    ldc 1           ; decrement
+    sub             ; left count - 1
+    stl 0           ; store count left
     br LOOP
 LOOP_OUT:
-times: SET 2
-increment: SET 3
 
-HAHA:
+times: SET 100      ; variable
 HALT
-result: data 0
+
+result: data 0      ; result is stored here
